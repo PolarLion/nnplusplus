d8bd271c90119a456c25eedbd78259ec713c3a16
RainAndSnow <545135082@qq.com>
2015-06-26 20:38:19 +0800

    haha

--- main.cpp
+++ main.cpp
@@ -9,9 +9,21 @@ using namespace std;
 int main (int argc, char** argv) 
 {
 	std::cout << "hello world\n";
-	NeuralNet n (3, 5, 2, 5, "negation", "logistic");
+	
+	char* pend = NULL;
+	int epoch = 1;
+	double learning_rate = 1;
+	if (2 == argc) {
+		epoch = strtol (argv[1], NULL, 10);
+	}
+	if (3 == argc) {
+		epoch = strtol (argv[1], NULL, 10);
+		learning_rate = strtod (argv[2], NULL);
+	}
+	NeuralNet n (epoch, learning_rate, 3, 2, 2, 1, "logistic", "logistic");
 	n.test ();
 	return 0;
+
 	vector<int> x;
 	for (int i = 0; i < 10; ++i) {
 		x.push_back (i);
